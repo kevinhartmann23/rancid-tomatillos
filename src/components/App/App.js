90cc769f322618d48a3511504d27c7c1c87492c1
Alia Peterson <alia@pnwebs.com>
2021-02-02 20:00:26 -0700

    Format files

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import greenTomato from '../../images/icon-tomato-green.png'
+import './App.css'
+
+function App() {
+  return (
+    <div className='App'>
+      <img src={greenTomato} />
+      <h1>RANCID TOMATILLOS</h1>
+    </div>
+  )
+}
+
+export default App
