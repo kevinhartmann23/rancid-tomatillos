87d3b96702490ba83aa26dd1e6e949d144abe8da
Kevin Hartmann <kevinhartmann23@gmail.com>
2021-02-03 16:15:46 -0700

    Display movie data on page correctly

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -4,7 +4,14 @@ import './Movies.css'
 
 function Movies({movies}){
  const movieCollection = movies.map(movie => {
-  return <Card />
+  return (
+   <Card 
+    id={movie.id}
+    poster={movie.poster_path}
+    title={movie.title}
+    rating={movie.average_rating}
+   />
+  )
  })
  return (
   <>
@@ -14,3 +21,12 @@ function Movies({movies}){
 }
 
 export default Movies
+
+// {
+//  "id": 337401,
+//   "poster_path": "https://image.tmdb.org/t/p/original//aKx1ARwG55zZ0GpRvU2WrGrCG9o.jpg",
+//    "backdrop_path": "https://image.tmdb.org/t/p/original//zzWGRw277MNoCs3zhyG3YmYQsXv.jpg",
+//     "title": "Mulan",
+//      "average_rating": 4.909090909090909,
+//       "release_date": "2020-09-04"
+// },
